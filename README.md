8a01d8bc44bdddb685665453dfca065ba9e05956
nikkehtine <nikkehtine@int.pl>
2025-01-11 00:45:50 +0100

    Setup

---
+++ README.md
@@ -0,0 +1,8 @@
+# Eureka
+
+Kahoot clone
+
+- Vue.js
+- Tailwind
+
+[Kahoot Quiz Clone in Svelte & Go [FULL SERIES] by Practical Coder](https://www.youtube.com/watch?v=gQdvg0uI1yg)
---
+++ client/src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+
+type RouteLink = { name: string; path: string };
+
+const routes: RouteLink[] = [
+  { name: "Home", path: "/" },
+  { name: "About", path: "/about" },
+];
+</script>
+
+<template>
+  <header class="flex items-center justify-between bg-gray-800 p-4 text-white">
+    <img alt="Vue logo" class="w-10" src="@/assets/logo.svg" />
+    <nav class="space-x-4">
+      <RouterLink v-for="route in routes" :to="route.path">{{ route.name }}</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
